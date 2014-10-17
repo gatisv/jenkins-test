992e9b38fd9955e111e50f36f3289d4f69775f17
Gatis Vilcans <gatis@mildagroup.com>
2014-10-17 14:07:56 +1100

    test app

---
+++ WPHubApp/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using WPHubApp.Resources;
+
+namespace WPHubApp
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
